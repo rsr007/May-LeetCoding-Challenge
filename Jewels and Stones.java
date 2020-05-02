d08762317a41409228e8b626dedf1883853111f2
Ravi Shankar Rai <rsr7294@gmail.com>
2020-05-02 19:47:23 +0530

    Update Jewels and Stones.java

--- Jewels and Stones.java
+++ Jewels and Stones.java
@@ -8,8 +8,9 @@ public static int numJewelsInStones(String J, String S) {
 			ans += cnt[c];
 		return ans;
 	}
+
 	/*
-	* Explanation:
+	  Explanation:
 	  ------------
 		Simply take an array of length 123 (since max ascii value of alphabets is of 'z'=122).
 		Make count of stones of different type and then check jewel array and add count of stones
